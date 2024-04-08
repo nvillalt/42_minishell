7c9f2de68aa1a0297a4dde11d3d43b2bf038553d
Francisco Jose Moran Mato <fmoran-m@c3r8s6.42madrid.com>
2024-04-08 19:47:49 +0200

    fix: edge cases -n flag in echo

--- built-ins/ft_echo.c
+++ built-ins/ft_echo.c
@@ -5,33 +5,9 @@
 /*   Created: 2024/04/08 13:35:19 by fmoran-m          #+#    #+#             */
 /*   Updated: 2024/04/08 16:04:45 by fmoran-m         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
 
+#include "../libft/libft.h"
 #include <stdio.h>
-
-static size_t	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-static int	ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	while ((s1[i] || s2[i]) && i < n)
-	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		i++;
-	}
-	return (0);
-}
 
 static int	count_argc(char **str)
 {
@@ -51,6 +27,11 @@ static void	ft_echo(char **str)
 
 	i = 1;
 	n_flag = 1;
+	if (str[1] == NULL)
+	{
+		printf("\n");
+		return;
+	}
 	argc = count_argc(str);
 	if (ft_strncmp(str[i], "-n", 2) == 0 && ft_strlen(str[i]) == 2)
 	{
@@ -62,7 +43,8 @@ static void	ft_echo(char **str)
 		printf("%s ", str[i]);
 		i++;
 	}
-	printf("%s", str[i]);
+	if (str[i])
+		printf("%s", str[i]);
 	if (n_flag != 0)
 		printf("\n");
 }
