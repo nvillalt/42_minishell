12f354587366696499c8cd4e19196370d13a7b8d
Francisco Jose Moran Mato <fmoran-m@c3r8s6.42madrid.com>
2024-04-08 22:34:50 +0200

    add: first try pwd

---
+++ src/built-ins/ft_pwd.c
@@ -0,0 +1,23 @@
+#include "../../minishell.h"
+
+int main(int argc, char **argv, char **env)
+{
+	char *str;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while(env[i] && ft_strncmp(env[i], "PWD", 3) != 0)
+		i++;
+	if (!env[i])
+		perror("error");
+	while(env[i][j])
+		j++;
+	str = getcwd(str, j - 4);
+	printf("%d\n", j - 4);
+	printf("%s\n", str);
+	free(str);
+	system("leaks -q a.out");
+	return 0;
+}
