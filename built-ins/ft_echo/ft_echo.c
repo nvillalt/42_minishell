dcd57edd88d23515216637a9168f19f864f8650a
Francisco Jose Moran Mato <fmoran-m@c3r5s4.42madrid.com>
2024-04-08 16:17:02 +0200

    add: control -n flag in echo

--- built-ins/ft_echo/ft_echo.c
+++ built-ins/ft_echo/ft_echo.c
@@ -1,18 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_echo.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fmoran-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 13:35:19 by fmoran-m          #+#    #+#             */
-/*   Updated: 2024/04/08 14:34:11 by fmoran-m         ###   ########.fr       */
+/*   Updated: 2024/04/08 16:04:45 by fmoran-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
+#include <stdio.h>
 
-static void c
+static size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+static int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+static int	count_argc(char **str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+		i++;
+	return(i);
+}
 
 static void	ft_echo(char **str)
 {
@@ -22,16 +51,18 @@ static void	ft_echo(char **str)
 
 	i = 1;
 	n_flag = 1;
+	argc = count_argc(str);
 	if (ft_strncmp(str[i], "-n", 2) == 0 && ft_strlen(str[i]) == 2)
 	{
 		i++;
 		n_flag = 0;
 	}
-	while(str[i])
+	while(i < argc - 1)
 	{
 		printf("%s ", str[i]);
 		i++;
 	}
+	printf("%s", str[i]);
 	if (n_flag != 0)
 		printf("\n");
 }
