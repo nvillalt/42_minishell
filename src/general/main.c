e317d7593915e628a6adff8434a6f320b338a000
Francisco Jose Moran Mato <fmoran-m@c3r6s6.42madrid.com>
2024-04-17 23:42:37 +0200

    Fix: I believe all built-in are fine, oldpwd is created, also minienv. SHLVL is hardcoded, to be implemented

--- src/general/main.c
+++ src/general/main.c
@@ -21,6 +21,55 @@ static char	**create_mini_env(void)
 	return (env);
 }
 
+static int	ft_strncmp_varlen(char *cmd, char *env_line)
+{
+	int	cmd_len;
+	int	var_len;
+
+	cmd_len = ft_strlen(cmd);
+	var_len = 0;
+	while(env_line[var_len] && env_line[var_len] != '=')
+		var_len++;
+	if (ft_strncmp(cmd, env_line, cmd_len) == 0 && cmd_len == var_len)
+		return(0);
+	else	
+		return (1);
+}
+
+static char	**set_oldpwd(char **env)
+{
+	int		i;
+	char	**new_env;
+
+	i = 0;
+	while(env[i] && ft_strncmp_varlen("OLDPWD", env[i]))
+		i++;
+	if (env[i])
+	{
+		free(env[i]);
+		env[i] = ft_strdup("OLDPWD");
+		return (env);
+	}
+	else
+	{
+		new_env = ft_calloc(i + 2, sizeof(char *));
+		i = 0;
+		while (env[i])
+		{
+			new_env[i] = ft_strdup(env[i]);
+			if (!new_env[i])
+			{
+				perror(NULL);
+				exit(1);
+			}
+			i++;
+		}
+		new_env[i] = ft_strdup("OLDPWD");
+		free_matrix(env);
+		return(new_env);
+	}
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	t_utils	utils;
@@ -32,7 +81,7 @@ int	main(int argc, char **argv, char **envp)
 		utils.env = env_dup(envp); // Aquí se aloja memoria. Liberarla más adelante.
 		utils.path = get_path(utils.env);
 	}
-//	utils.env = set_oldpwd(utils.env);
+	utils.env = set_oldpwd(utils.env);
 	prompt_loop(&utils);
 	// Función para liberar al final ????
 	free_matrix(utils.env);
