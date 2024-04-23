c85133be48bc48925e18bf2f0750aff2a0024a25
fmoran-m <franmoranmato@gmail.com>
2024-04-23 14:25:05 +0200

    add: Free utils update

--- src/general/free_utils.c
+++ src/general/free_utils.c
@@ -14,10 +14,54 @@ void	free_matrix(char **matrix)
 	return ;
 }
 
+void	free_redir_list(t_parse *process) //Libera todos los redir de todos los nodos de parse
+{
+	t_redir	*temp;
+
+	while(process)
+	{
+		while(process->redirec)
+		{
+			if (process->redirec->doc)
+				free(process->redirec->doc);
+			temp = process->redirec;
+			process->redirec = process->redirec->next;
+			free(temp);
+		}
+		process = process->next;
+	}
+}
+
+void	free_parse_list(t_parse	*process)
+{
+	t_parse	*temp;
+
+	while(process)
+	{
+		if(process->cmd)
+			free_matrix(process->cmd); //No se libera bien hasta hacer una reserva correcta
+		temp = process;
+		process = process->next;
+		free(temp);
+	}
+}
+
 void	free_utils(t_utils *utils)
 {
 	if (utils->env)
 		free_matrix(utils->env);
 	if (utils->path)
 		free_matrix(utils->path);
+	if (utils->process->redirec)
+		free_redir_list(utils->process);
+	if (utils->process)
+		free_parse_list(utils->process);
+}
+
+void	free_lists(t_utils	*utils)
+{
+	if (utils->process->redirec)
+		free_redir_list(utils->process);
+	if (utils->process)
+		free_parse_list(utils->process);
 }
