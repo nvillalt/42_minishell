60218610e42f71411fe6dbe4a30b5f02dcbd3db6
Francisco Jose Moran Mato <fmoran-m@c1r6s4.42madrid.com>
2024-06-03 19:53:55 +0200

    fix signal number

--- src/signals/sigint_signal.c
+++ src/signals/sigint_signal.c
@@ -6,7 +6,7 @@
 /*   By: fmoran-m <fmoran-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 18:47:03 by fmoran-m          #+#    #+#             */
-/*   Updated: 2024/06/03 18:51:30 by fmoran-m         ###   ########.fr       */
+/*   Updated: 2024/06/03 19:46:11 by fmoran-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 static void	exec_sigint(int signal)
 {
-	write(STDERR_FILENO, "\n", 0);
+	write(STDERR_FILENO, "\n", 1);
 	if (signal == SIGINT)
 	{
 		rl_on_new_line();
-		rl_replace_line("", -1);
+		rl_replace_line("", 0);
 		rl_redisplay();
 	}
 }
